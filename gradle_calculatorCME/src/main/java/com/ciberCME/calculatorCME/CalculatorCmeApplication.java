151f95c048a8ff32c17dda38b7adb882cdc9ba5e
Enrique Carballo Miguel <tez20778@educastur.es>
2026-06-02 11:59:53 +0000

    Añadido esqueleto Spring Boot

---
+++ gradle_calculatorCME/src/main/java/com/ciberCME/calculatorCME/CalculatorCmeApplication.java
@@ -0,0 +1,13 @@
+package com.ciberCME.calculatorCME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCmeApplication.class, args);
+	}
+
+}
---
+++ gradle_calculatorCME/src/test/java/com/ciberCME/calculatorCME/CalculatorCmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberCME.calculatorCME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorCmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
